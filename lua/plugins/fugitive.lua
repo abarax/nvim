d57518b4a1991c6b9c6c34fe34c2920f367b6090
Leigh Appel <leigh.appel@gmail.com>
2024-06-01 12:05:03 +1000

    Added git plugins and supermaven

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  "tpope/vim-fugitive",
+  config = function()
+    vim.g.fugitive_git_executable = "/usr/bin/git"
+    vim.g.fugitive_git_executable = "/usr/bin/git"
+  end
+}
---
+++ lua/plugins/vim-gitgutter.lua
@@ -0,0 +1,16 @@
+return {
+  "airblade/vim-gitgutter",
+  config = function()
+    vim.g.gitgutter_enabled = true
+    vim.g.gitgutter_sign_highlight_priority = 100
+    vim.g.gitgutter_highlight_lines = true
+    vim.g.gitgutter_highlight_columns = true
+    vim.g.gitgutter_highlight_bookmarks = true
+    vim.g.gitgutter_highlight_added_lines = true
+    vim.g.gitgutter_highlight_removed_lines = true
+    vim.g.gitgutter_highlight_statusline = true
+    vim.g.gitgutter_map_keys = true
+    vim.g.gitgutter_override_mappings = true
+    vim.g.gitgutter_max_signs = 250
+  end
+}
