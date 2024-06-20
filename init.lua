b11c29117bc0eb6539570cc2e12ca518ffd461e5
Leigh Appel <leigh.appel@gmail.com>
2024-06-20 16:17:39 +1000

    Fix up telescope

--- init.lua
+++ init.lua
@@ -15,25 +15,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
-local builtin = require('telescope.builtin')
-require('telescope').setup{
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        ["<C-j>"] = "move_selection_next",
-        ["<C-k>"] = "move_selection_previous",
-
-      }
-    }
-  },
-  pickers = { },
-  extensions = { }
-}
-vim.keymap.set('n', '<leader>tff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>tfg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>tfb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>tfh', builtin.help_tags, {})
-
